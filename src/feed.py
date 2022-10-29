ee52ec4e5f2af0c49a9def3897c5853e600f0876
Kei Shimizu <suns.manage.k@gmail.com>
2022-10-30 08:20:08 +0900

    avoid invalid path due to non-existent dir

--- src/feed.py
+++ src/feed.py
@@ -1,6 +1,9 @@
+import os
 from reader import make_reader
 
 def setup_feed(db, url):
+    setup_dir_for_file_path(db)
+    
     try:
       reader = make_reader(db)
       reader.add_feed(url)
@@ -8,3 +11,11 @@ def setup_feed(db, url):
       print(e)
     finally:
       return reader
+    
+
+def setup_dir_for_file_path(file_path: str):
+    create_dir_if_not_exists('/'.join(file_path.split('/')[:-1]))
+
+
+def create_dir_if_not_exists(path):
+    os.makedirs(path, exist_ok=True)
