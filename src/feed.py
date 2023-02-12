da178206715ac75b06a21fb657d89795e10162ee
Kei Shimizu <suns.manage.k@gmail.com>
2023-02-12 11:55:39 +0900

    removed error handling

--- src/feed.py
+++ src/feed.py
@@ -3,14 +3,9 @@ from reader import make_reader
 
 def setup_feed(db, url):
     setup_dir_for_file_path(db)
-    
-    try:
-      reader = make_reader(db)
-      reader.add_feed(url)
-    except Exception as e:
-      print(e)
-    finally:
-      return reader
+    reader = make_reader(db)
+    reader.add_feed(url)
+    return reader
     
 
 def setup_dir_for_file_path(file_path: str):
